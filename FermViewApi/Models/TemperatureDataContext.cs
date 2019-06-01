fe9b20fa5612f9cd8e6273d340af30432b73f7cf
wayfrae <frazier.zach@gmail.com>
2019-05-31 19:48:32 -0600

    added models and controllers

---
+++ FermViewApi/Models/TemperatureDataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FermViewApi.Models;
+
+namespace FermViewApi.Models
+{
+    public class TemperatureDataContext : DbContext
+    {
+       public TemperatureDataContext(DbContextOptions<TemperatureDataContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TemperatureData>();
+        }
+
+
+        public DbSet<TemperatureData> Temperatures { get; set; }
+
+
+        public DbSet<FermViewApi.Models.Brew> Brew { get; set; }
+
+        
+    }
+}
---
+++ FermViewApi/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FermViewApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace FermViewApi
+{
+    public class Startup
+    {
+        public static string ConnectionString { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            ConnectionString = Configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TemperatureDataContext>(opt =>
+               opt.UseInMemoryDatabase("TemperatureData"));
+            //opt.UseSqlServer(ConnectionString));
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                 options.Authority = "https://dev-750345.oktapreview.com/oauth2/default";
+                 options.Audience = "api://default";
+                 options.RequireHttpsMetadata = false;
+                });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
